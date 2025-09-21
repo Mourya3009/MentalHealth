5d4eedbd86a3b1202ec3a026bae7245a4996f5af
Mourya Ayyappa <mouryaayyappa@gmail.com>
2025-09-21 17:17:14 +0530

    done4

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -112,7 +112,7 @@ const Login = () => {
               />
             </div>
 
-            <div>
+            {/* <div>
               <label htmlFor="captcha" className="block text-sm font-medium text-slate-300 mb-2">
                 Captcha Verification
               </label>
@@ -132,8 +132,8 @@ const Login = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
                   </svg>
                 </button>
-              </div>
-              <input
+              </div> */}
+              {/* <input
                 type="text"
                 id="captcha"
                 name="captcha"
@@ -143,7 +143,7 @@ const Login = () => {
                 placeholder="Enter the captcha above"
                 required
               />
-            </div>
+            </div> */}
 
             <button
               type="submit"
